2d165341f34b1128f325f9080164ec8a2a22097a
전윤호 <wjse0213@naver.com>
2022-06-15 17:51:43 +0900

    build:menuRestAPI

--- order/urls.py
+++ order/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from order import views
 
 urlpatterns=[
-    path('shops/',views.shop)
+    path('shops/',views.shop,name="shop"),
+    path('menus/',views.menu, name="menu"),
 ]
