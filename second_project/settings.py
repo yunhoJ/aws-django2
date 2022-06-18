80aa84501fd0e82eb594d6bddb0f9e3ccb3d522c
전윤호 <wjse0213@naver.com>
2022-06-19 01:07:06 +0900

    AWS RDS 연결 후 배포

--- second_project/settings.py
+++ second_project/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'second_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yhproject1',
+        'USER':'admin',
+        'PASSWORD':'coontec2021',
+        'HOST':'database-2.cqtbwwsb4icy.us-east-1.rds.amazonaws.com',
+        'PORT':'3306',
+        'OPTIONS':{
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
